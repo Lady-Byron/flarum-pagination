bd5ad43b7f57e9a7b2a5e289acad4e577b8e3add
lrysia <enhome2012@gmail.com>
2023-02-23 18:25:07 +0800

    Fix a bug: Search results can only be one page at most

--- src/Overrides/Search/AbstractSearcher.php
+++ src/Overrides/Search/AbstractSearcher.php
@@ -14,6 +14,8 @@ use Flarum\Query\QueryCriteria;
 use Flarum\Query\QueryResults;
 use Flarum\User\User;
 use Illuminate\Database\Eloquent\Builder;
+// added by lrysia/pagination
+use Illuminate\Database\Query\Builder as QBulider;
 
 abstract class AbstractSearcher
 {
@@ -56,7 +58,25 @@ abstract class AbstractSearcher
 
 
         // added by lrysia/pagination
-        $totalResultsCount = $query->count();
+        // 1. $query是Eloquent/builder，调用 getQuery() 返回的是个 Query/builder
+        // 2. 直接用 $query->count()时，实为 Query/builder->count()
+        // 3. Query/builder->count() 再次调用 Query/builder->aggregate()
+        // 4. 在"搜索"情景下，aggregate() 的返回结果不是我们需要的
+        // 5. 在"搜索"情景下，aggregate() 中的 $result->items 会变成包含多个对象的列表
+        // 6. 而在"filter"情境下，$result->items 始终是只包含一个对象的列表
+        // 7. 在"搜索"情景下，列表中的每个对象代表一个帖子，而每个对象的count数代表该帖子内符合查询条件的条目数
+        // 8. 在"filter"情境下，列表只有一个对象，这个对象的count数就是符合查询条件的帖子数
+        // 9. 观察到 aggregate() 的 return 硬编码了“[0]”，这导致返回值始终是列表内第一个对象的count数
+        // 10. 基于避免修改原函数的原则，这里给 Query/builder 添加了一个能正确返回"搜索"情景下count数的新方法
+        QBulider::macro( 'countForSearch', function ($function='count',$columns=['*']) {
+            $results = $this->cloneWithout($this->unions || $this->havings ? [] : ['columns'])
+                ->cloneWithoutBindings($this->unions || $this->havings ? [] : ['select'])
+                ->setAggregate($function, $columns)
+                ->get($columns);
+            return $results;
+        });
+        $q_builder = $query->getQuery();
+        $totalResultsCount = count($q_builder->countForSearch()->all());
 
 
         $this->applySort($search, $criteria->sort, $criteria->sortIsDefault);
@@ -67,10 +87,12 @@ abstract class AbstractSearcher
             $mutator($search, $criteria);
         }
 
+
         // Execute the search query and retrieve the results. We get one more
         // results than the user asked for, so that we can say if there are more
         // results. If there are, we will get rid of that extra result.
         $results = $query->get();
+
 
         if ($areMoreResults = $limit > 0 && $results->count() > $limit) {
             $results->pop();
