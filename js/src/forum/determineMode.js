057763af4ddff63c873bba5e1850c366d9f6227b
lrysia <enhome2012@gmail.com>
2023-02-23 23:42:30 +0800

    Fix a bug: Unable to load forum pages as a guest.

--- js/src/forum/determineMode.js
+++ js/src/forum/determineMode.js
@@ -3,21 +3,22 @@ import app from 'flarum/forum/app';
 
 export default function () {
 
+  if(app.session.user === null) {
+    return Boolean(app.forum.attribute('LrysiaPagination.PaginationFirst'));
+  }
+
   if (Boolean(app.forum.attribute('LrysiaPagination.CanUserPref'))) {
     if (!Boolean(app.session.user.preferences().DLP_UserCustom)) {
-      if (!Boolean(app.forum.attribute('LrysiaPagination.PaginationFirst'))) {
-        return false;
-      }
+      return Boolean(app.forum.attribute('LrysiaPagination.PaginationFirst'));
     } else {
       if (!Boolean(app.session.user.preferences().DLP_UserPaginationFirst)) {
         return false;
       }
     }
   } else {
-    if (!Boolean(app.forum.attribute('LrysiaPagination.PaginationFirst'))) {
-      return false;
-    }
+    return Boolean(app.forum.attribute('LrysiaPagination.PaginationFirst'));
   }
 
   return true;
 }
+
